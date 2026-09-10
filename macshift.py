15651974b4eae4a27b8079e8f77eda4381882b6d
Joel-C-P <Joel-C-P>
2026-09-10 11:27:06 -0400

    Handle Ctrl+C interruption with signal()

--- macshift.py
+++ macshift.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3 
 
+import sys
+import signal
+import time
+
+def def_handler(sig, frame):
+
+    print(f"Getting out...")
+    sys.exit(1);
+
+signal.signal(signal.SIGINT, def_handler)
+
+def change_mac():
+    
+    time.sleep(2)
+    print("works")
+
 def main():
+
+    change_mac()
 
 if __name__=='__main__':
 
