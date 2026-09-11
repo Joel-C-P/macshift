fe4bb00a6b99801bbc3475853d19361dd077e353
Joel-C-P <Joel-C-P>
2026-09-11 08:35:06 -0400

    Getting two arguments

--- macshift.py
+++ macshift.py
@@ -3,6 +3,16 @@
 import sys
 import signal
 import time
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Change MAC address")
+    parser.add_argument("-m", "--mac", dest= "mac_address", required=True, help= "Ex: -m xx:xx:xx:xx:xx:xx")
+    parser.add_argument("-i", "--interface", dest="interface", required=True, help="Ex: -i eth0|ens34")
+
+    options = parser.parse_args()
+
+    return options.mac_address, options.interface
 
 def def_handler(sig, frame):
 
@@ -11,15 +21,18 @@ def def_handler(sig, frame):
 
 signal.signal(signal.SIGINT, def_handler)
 
+
 def change_mac():
     
-    time.sleep(2)
-    print("works")
-
+    print()
 def main():
 
     change_mac()
+    
+    mac_address, interface = get_arguments()
 
+    print(mac_address)
+    print(interface)
 if __name__=='__main__':
 
     main()
